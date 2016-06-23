23a80a91770afc3b0e9b1c8fb99a55c85216879c
bandreghetti <brunoandreghetti@gmail.com>
2016-06-23 10:29:49 -0300

    Código Incompleto do controle
    Já tem umas funções de motores e o início do PID pra controlar a
    velocidade dos motores pra frente. 23/06/2016

---
+++ pateta.c
@@ -0,0 +1,106 @@
+#define TRIGGER 5
+#define TRIG_DELAY 100
+#define	FRENTE 4
+#define TRAS 7
+#define LADO_1 
+#define LADO_2 
+#define LDR_1 A4
+#define LDR_2 A5
+#define LDR_3 A6
+#define LDR_4 A7
+#define ESQUERDA -1
+#define DIREITA 1
+
+
+void iniciaSensorLDR(int analogpin)
+{
+	pinMode(analogpin, INPUT);
+}
+
+void iniciaSensorUS(byte echo)
+{
+	pinMode(TRIGGER, OUTPUT);
+	pinMode(echo, INPUT);
+}
+
+int SensorLDR(byte qualsensor) // passar LDR_N sendo N = 1, 2, 3 ou 4
+{
+	return analogRead(qualsensor);
+}
+
+int SensorUS(byte qualsensor) // passar FRENTE ou TRAS como parâmetro
+{
+	digitalWrite(TRIGGER, LOW);
+	delayMicroseconds(2);
+	digitalWrite(TRIGGER, HIGH); // ativa o sensor HC-SR04 com um pulso de 5 microssegundos no pino Trigger
+	delayMicroseconds(5);
+	digitalWrite(TRIGGER, LOW);
+  
+	unsigned long duration = pulseIn(qualsensor, HIGH); // calcula o tempo necessário para o retorno do pulso sonoro
+	int distanceCentimeters = (duration * 17)/1000; // calcula a distância percorrida pelo pulso sonoro
+	
+	delay(TRIG_DELAY);
+	return distanceCentimeters; // retorna o valor encontrado
+}
+
+int tanalinha(byte qual_ldr)
+{
+	if(SensorLDR(qual_ldr) < COR_LINHA)
+	{
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void desvia(byte lado)
+{
+	digitalWrite(ENABLE, HIGH);
+	if(lado == ESQUERDA)
+	{
+		analogWrite(DESVIA_ESQUERDA, POT_DESVIO);
+		while(!tanalinha(LDR_1));
+		delay(TEMPO_PASSA_LINHA);
+		while(!tanalinha(LDR_1));
+		digitalWrite(DESVIA_ESQUERDA, LOW);
+	} else {
+		analogWrite(DESVIA_DIREITA, POT_DESVIO);
+		while(!tanalinha(LDR_2));
+		delay(TEMPO_PASSA_LINHA);
+		while(!tanalinha(LDR_2));
+		digitalWrite(DESVIA_DIREITA, LOW);
+	}
+}
+
+void tocaobarco()
+{
+	int tempo = 0, tempo_ant = 0;
+	int p, d;
+	int erro = 0, erro_ant;
+	
+	while(SensorUS(FRENTE) > DIST_OBSTACULO)
+	{		
+		erro_ant = erro;
+		erro = SensorUS(TRAS) - DIST_FUNDO;
+		tempo_ant = tempo;
+		tempo = millis();
+		
+		p = -erro * KPROP;
+		d = (erro_ant - erro) * KDERIV / (tempo - tempo_ant);
+		
+		// completar
+	}
+}
+
+
+void setup()
+{
+	iniciaSensorUS();
+	iniciaSensorLDR();
+}
+
+void loop()
+{
+	int distancia = SensorUS(FRENTE); // lê a distância no ultrassom da frente
+	int ldr_1 = SensorLDR(LDR_1); // lê a distância no LDR_1
+}
